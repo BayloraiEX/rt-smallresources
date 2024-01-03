2dfcbb0e705e4a340b72988702d6d6406aaead5a
BayloraiEX <117157428+BayloraiEX@users.noreply.github.com>
2024-01-03 14:10:48 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,25 +2,33 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'RT-smallresources | A Heavily Edited QB-SmallResources'
-version '1.0'
+version '1.0.1'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
     'locales/en.lua',
-    'config.lua'
+    'config.lua',
+    'main.lua',
 }
+
+ui_page('html/index.html')
+
 server_script 'server/*.lua'
+
 client_scripts {
     '@PolyZone/client.lua',
     '@PolyZone/BoxZone.lua',
     '@PolyZone/ComboZone.lua',
-    'client/*.lua'
+    'client/*.lua',
 }
 
 data_file 'FIVEM_LOVES_YOU_4B38E96CC036038F' 'events.meta'
 
 files {
-    'events.meta'
+    'events.meta',
+    'html/index.html',
+    'html/app.js',
+    'html/style.css',
 }
 
 lua54 'yes'
